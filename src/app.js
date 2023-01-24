c0bf14f8b1c664092776972c148b96cbebea12d6
Serge MUGISHA <mugishasergebonheur@gmail.com>
2023-01-24 00:57:43 -0500

    Update dependencies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 /**
- * Research Assignment 2
  * This Program reads data from a csv file, parses it,
  * and store the filtered records in an array of objects which is later outputed on the screen
  * Provides CRUD functionalities on the parsed data through the terminal
